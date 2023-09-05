0c767606d8956911974bafdc6ab20acc2e5b5dee
vamseemanchineni <vamsimanchineni26@gmail.com>
2023-09-05 10:50:46 +0530

    End of section 6

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Route, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,21 +14,20 @@ export class NavComponent implements OnInit{
   model : any = {};
  // CurrentUser$ : Observable<User | null> = of(null);
 
-  constructor(public accountService : AccountService) {}
+  constructor(public accountService : AccountService, private router: Router, private toastr: ToastrService) {}
   ngOnInit(): void {
     //this.CurrentUser$ = this.accountService.currentUser$;
   }
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next:() =>{
-        //console.log(response);
-      },
-      error: error => console.log(error)
+      next:() =>this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logout(){
+    this.router.navigateByUrl('/')
     this.accountService.logout();
   }
 
--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {}
 
-  constructor (private accountservice : AccountService) {}
+  constructor (private accountservice : AccountService, private toastr: ToastrService) {}
   ngOnInit(): void {
     
   }
@@ -22,7 +23,7 @@ export class RegisterComponent implements OnInit {
       next: () => {
         this.cancel();
       },
-      error:error=>console.log(error)
+      error:error=> this.toastr.error(error.error)
   })
   }
   cancel(){
