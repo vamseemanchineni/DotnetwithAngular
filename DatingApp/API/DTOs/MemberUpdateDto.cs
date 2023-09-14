d37c7bb6d77125bac4a739b3ba2f7f58f7d00d91
vamseemanchineni <vamsimanchineni26@gmail.com>
2023-09-15 01:19:32 +0530

    End Of Section 9

---
+++ DatingApp/API/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,13 @@
+namespace API.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Intrests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        
+
+    }
+}
